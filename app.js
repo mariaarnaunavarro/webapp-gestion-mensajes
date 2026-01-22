f3fc4676963f41f296e3db5285a0b9eabe02dc78
mariaarnaunavarro <135965676+mariaarnaunavarro@users.noreply.github.com>
2026-01-22 14:22:28 +0100

    Update app.js
    Añadida bd y el puerto

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ const db = mysql.createConnection({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
-  port: process.env.MYSQL_PORT || 3306
+  database: process.env.MYSQL_DATABASE,
+  port: process.env.MYSQL_PORT || 12345
 });
 
 db.connect(err => {
